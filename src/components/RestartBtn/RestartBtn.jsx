42747646b4f8a51ffcd5283ed2534ffedb5088d5
Mario <mario_bianchi_jr@hotmail.com>
2025-09-17 10:43:10 +0200

    Made a restart-button

---
+++ src/components/RestartBtn/RestartBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RestartBtn.module.css';
+
+const RestartBtn = () => {
+  return (
+    <button type='button' className={styles.RestartBtn}>
+      Börja om
+    </button>
+  );
+};
+
+export default RestartBtn;
