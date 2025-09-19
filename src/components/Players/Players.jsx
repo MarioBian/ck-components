99a22d39b923cb3477c0c5015a7f662d3eada0cc
Lehmol <chalottelinnea@hotmail.com>
2025-09-19 10:57:02 +0200

    Player selection function

---
+++ src/components/Players/Players.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from 'Players.module.css';
+
+const Piece = ({ color }) => (
+	<div className={color === 'red' ? styles.option1 : styles.option2}></div>
+);
+
+export default function PlayerColorSelector() {
+	const [player1Color, setPlayer1Color] = useState('');
+	const [player2Color, setPlayer2Color] = useState('');
+	const [currentPlayer, setCurrentPlayer] = useState('');
+	const [gameReady, setGameReady] = useState(false);
+
+	const handlePlayerSelect = (playerNumber) => {
+		setCurrentPlayers(playernumber);
+		if (playerNumber === 1) {
+			setPlayer1Color('');
+			setPlayer2Color('');
+		}
+		setGameReady(false);
+	};
+
+	const handleColorSelect = (color) => {
+		if (currentPlayer === 1) {
+			setPlayer1Color(color);
+			setPlayer2Color(color === 'red' ? 'blue' : 'red');
+			setGameReady(true);
+		} else (currentPlayer === 2) {
+			setPlayer2Color(color);
+			setPlayer1Color(color === 'red' ? 'blue' : 'red');
+			setGameReady(true);
+		}
+	};
+
+	return(
+		<div>
+			<h1>Player setup</h1>
+			{!currentPlayer && (
+				<div>
+					<h2> Choose a player </h2>
+					<div>
+						<button>Player 1</button>
+						<button>Player 2</button>
+					</div>
+				</div>
+			)}
+
+		</div>
+	)
+}
