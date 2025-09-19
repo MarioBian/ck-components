7073ccbc313f64cef2c1fdf62448c73033a9f96e
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-09-19 13:59:40 +0200

    röd icon/piece adderad

---
+++ src/components/RedPiece/RedPiece.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './RedPiece.module.css';
+
+const RedPiece = () => {
+	return (
+		<div className={styles.redpiece} >Röd</div>
+	)
+}
+
+export default RedPiece;
