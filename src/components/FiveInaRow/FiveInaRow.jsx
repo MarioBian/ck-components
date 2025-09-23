52eeb065b986c0c957b77fda13b02d3b20b59b1a
Mario <mario_bianchi_jr@hotmail.com>
2025-09-23 15:13:38 +0200

    logic for columns and looking forward

--- src/components/FiveInaRow/FiveInaRow.jsx
+++ src/components/FiveInaRow/FiveInaRow.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './FiveInaRow.module.css';
 
 
-const FiveInaRow = () => {
+const FiveInaRow = ({
+gameBoard = [],
+lastMoveRow = null,
+lastMoveColumn = null,
+activePlayer = null,
+onPlayerWin = () => {},
+}) => {
+
+useEffect(( => {
+if (!gameBoard.lenght || lastMoveRow === null ) return;
+
+const directions = [
+	[0, 1],
+	[1, 0],
+	[1, 1],
+	[1, -1],
+];
+
+const checkDirection = (rowStep, colStep) => {
+	let count = 1;
+
+	for (let i = 1; i < 5; i++) {
+	const row = lastMovedRow + rowStep * i;
+	const col = lastMoveColumn + colStep * i;
+	if (gameBoard[row]?.[col] === activePlayer) count ++;
+	else brak;
+}
+
+
 	
 	return (
 		<>Five in a row component</>
