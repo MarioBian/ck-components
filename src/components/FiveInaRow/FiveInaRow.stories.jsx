a81370eaf8c6a16670fd3319a174e1922640b65a
Mario <mario_bianchi_jr@hotmail.com>
2025-09-23 15:39:42 +0200

    made it work

--- src/components/FiveInaRow/FiveInaRow.stories.jsx
+++ src/components/FiveInaRow/FiveInaRow.stories.jsx
@@ -8,13 +8,5 @@ export default {
 
 }
 
-export const Default = () => (
-<FiveInaRow
-    gameBoard={Array(10).fill(null).map(() => Array(10).fill(null))}
-    lastMoveRow={0}
-    lastMoveColumn={0}
-    activePlayer="Red"
-    onPlayerWin={(player) => console.log(player + " won!")}
-  />
-);
+export const Default = {}
 
