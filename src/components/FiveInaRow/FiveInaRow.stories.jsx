c37d21d388bc27bb0944e5f88145b02d79a98e3f
Mario <mario_bianchi_jr@hotmail.com>
2025-09-23 15:28:41 +0200

    fixed stories

--- src/components/FiveInaRow/FiveInaRow.stories.jsx
+++ src/components/FiveInaRow/FiveInaRow.stories.jsx
@@ -8,14 +8,13 @@ export default {
 
 }
 
-export const Default ={}import {React} from 'react';
-import {FiveInaRow} from '.';
+export const Default = () => (
+<FiveInaRow
+    gameBoard={Array(10).fill(null).map(() => Array(10).fill(null))}
+    lastMoveRow={0}
+    lastMoveColumn={0}
+    activePlayer="Red"
+    onPlayerWin={(player) => console.log(player + " won!")}
+  />
+);
 
-export default {
-
-        title:'components/FiveInaRow',
-        component: FiveInaRow,
-
-}
-
-export const Default ={}
