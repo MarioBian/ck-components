534f602bd9514b51de5685095220be82557316ce
Lehmol <chalottelinnea@hotmail.com>
2025-09-30 09:55:13 +0200

    updating Piece to be able to connect to Players

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import styles from './Piece.module.css';
 
-const Piece = () => 
-<div 
-  className={styles.bluePiece}
-  draggable
-  onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
-  >
-    blå
-</div>;
-
-<div 
-  className={styles.redPiece}
-  draggable
-  onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
-  >
-    Röd
-</div>;
+const Piece = ({ color }) =>{
+	if (color === 'red') {
+		return(
+			<div
+				className={styles.redPiece}
+				draggable
+				onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
+			>
+				Röd
+			</div>
+		);
+	} else if (color === 'blue') {
+		return(
+			<div
+				className={styles.bluePiece}
+				draggable
+				onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
+			>
+				Blå
+			</div>
+		);
+	}
+	return null;
+};
 
 export default Piece;
 
