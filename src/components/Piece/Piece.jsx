0bb24f112c4682cf13d33e63f286e70ab9ecba21
Mario <mario_bianchi_jr@hotmail.com>
2025-10-02 11:09:06 +0200

    commented awai the first fun

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
-import styles from './Piece.module.css';
+import React from "react";
+import styles from "./Piece.module.css";
 
-const Piece = () => {
-return (
- <div className={styles.piecesContainer}>
-<div 
-  className={styles.bluePiece}
-  draggable
-  onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
-  >
-    blå
-</div>;
+// const Piece = () => {
+// return (
+//  <div className={styles.piecesContainer}>
+// <div
+//   className={styles.bluePiece}
+//   draggable
+//   onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
+//   >
+//     blå
+// </div>;
 
-<div 
-  className={styles.redPiece}
-  draggable
-  onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
-  >
-    Röd
-</div>;
-</div>
-)
-}
-const Piece = ({ color }) =>{
-	if (color === 'red') {
-		return(
-			<div
-				className={styles.redPiece}
-				draggable
-				onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
-			>
-				Röd
-			</div>
-		);
-	} else if (color === 'blue') {
-		return(
-			<div
-				className={styles.bluePiece}
-				draggable
-				onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
-			>
-				Blå
-			</div>
-		);
-	}
-	return null;
+// <div
+//   className={styles.redPiece}
+//   draggable
+//   onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
+//   >
+//     Röd
+// </div>;
+// </div>
+// )
+// }
+const Piece = ({ color }) => {
+  if (color === "red") {
+    return (
+      <div
+        className={styles.redPiece}
+        draggable
+        onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
+      >
+        Röd
+      </div>
+    );
+  } else if (color === "blue") {
+    return (
+      <div
+        className={styles.bluePiece}
+        draggable
+        onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
+      >
+        Blå
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Piece;
-
