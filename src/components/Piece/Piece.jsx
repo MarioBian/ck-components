18bf864b550c18e15a765ff18406fab943c90f33
Mario <mario_bianchi_jr@hotmail.com>
2025-10-02 11:13:21 +0200

    Commented Var

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -1,30 +1,17 @@
 import React from "react";
 import styles from "./Piece.module.css";
 
-// const Piece = () => {
-// return (
-//  <div className={styles.piecesContainer}>
-// <div
-//   className={styles.bluePiece}
-//   draggable
-//   onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
-//   >
-//     blå
-// </div>;
-
-// <div
-//   className={styles.redPiece}
-//   draggable
-//   onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
-//   >
-//     Röd
-// </div>;
-// </div>
-// )
-// }
-const Piece = ({ color }) => {
-  if (color === "red") {
-    return (
+const Piece = () => {
+  return (
+    <div className={styles.piecesContainer}>
+      <div
+        className={styles.bluePiece}
+        draggable
+        onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
+      >
+        blå
+      </div>
+      ;
       <div
         className={styles.redPiece}
         draggable
@@ -32,19 +19,33 @@ const Piece = ({ color }) => {
       >
         Röd
       </div>
-    );
-  } else if (color === "blue") {
-    return (
-      <div
-        className={styles.bluePiece}
-        draggable
-        onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
-      >
-        Blå
-      </div>
-    );
-  }
-  return null;
+      ;
+    </div>
+  );
 };
+// const Piece = ({ color }) => {
+//   if (color === "red") {
+//     return (
+//       <div
+//         className={styles.redPiece}
+//         draggable
+//         onDragStart={(e) => e.dataTransfer.setData("piece", "red")}
+//       >
+//         Röd
+//       </div>
+//     );
+//   } else if (color === "blue") {
+//     return (
+//       <div
+//         className={styles.bluePiece}
+//         draggable
+//         onDragStart={(e) => e.dataTransfer.setData("piece", "blue")}
+//       >
+//         Blå
+//       </div>
+//     );
+//   }
+//   return null;
+// };
 
 export default Piece;
