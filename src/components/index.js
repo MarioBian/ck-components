fd85f4c087d8bb348f2b62e4ce74695dcdc3b262
Lehmol <chalottelinnea@hotmail.com>
2025-10-06 10:35:43 +0200

    exported Players

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Button } from "./components/Button";
 export { Gameboard } from "./components/Gameboard";
 export { Background } from "./components/Background";
+export { Players } from "./components/Players";
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as Button } from "./components/Button/Button.jsx";
 export { default as GameBoard } from "./components/GameBoard/Gameboard.jsx";
 export { default as Piece } from "./components/Piece/Piece.jsx";
 export { default as Background } from "./components/Background/Background.jsx";
+export { default as Players } from "./components/Players/Players.jsx";
