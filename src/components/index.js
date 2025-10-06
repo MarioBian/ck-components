b20ad0b2308b5fd74e492ae02ee07ea8447dc35f
Lehmol <chalottelinnea@hotmail.com>
2025-10-06 10:48:18 +0200

    exported Piece

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { Button } from "./components/Button";
 export { Gameboard } from "./components/Gameboard";
 export { Background } from "./components/Background";
 export { Players } from "./components/Players";
+export { Piece } from "./components/Piece";
