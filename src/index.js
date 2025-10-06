013308d86c92d5a4b29517ebcd9cb5d8d649da01
Lehmol <chalottelinnea@hotmail.com>
2025-10-06 10:53:52 +0200

    fixed bug in export

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ export { default as GameBoard } from "./components/GameBoard/Gameboard.jsx";
 export { default as Piece } from "./components/Piece/Piece.jsx";
 export { default as Background } from "./components/Background/Background.jsx";
 export { default as Players } from "./components/Players/Players.jsx";
-export { default as Piece } from "./components/Piece/Piece.jsx";
