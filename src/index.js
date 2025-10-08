aeea463d3b5fe852ced60b50315fda04037e5378
mariaMerethe <borglundmaria@gmail.com>
2025-10-08 11:41:32 +0200

    Header med png-fil

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as Background } from "./components/Background/Background.jsx";
 export { default as Players } from "./components/Players/Players.jsx";
 export { default as PopUp } from "./components/PopUp/PopUp.jsx";
 export { default as FiveInaRow } from "./components/FiveInaRow/FiveInaRow.jsx";
+export { default as Header } from "./components/Header/Header.jsx";
